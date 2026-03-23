94c08e515f6bc13e4c6cb43feb5b2bcb090ec14f
GregoryV <vxdll.39@hotmail.com>
2026-03-23 23:35:56 +0200

    First steprs

--- Base/Program.cs
+++ Base/Program.cs
@@ -1,2 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("What is you'r name? ");
+var name = Console.ReadLine();
+Console.WriteLine($"Hello {name}");
